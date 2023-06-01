4e4c4f931d46120f4f11c02148d461804ad922b3
minjun._.j <126859900+wjsalswns@users.noreply.github.com>
2023-06-01 17:40:44 +0900

    Create Ex11.java

---
+++ Ex11.java
@@ -0,0 +1,37 @@
+package test;
+
+public class Ex11 {
+	public static void main(String[] args) {
+		// 1부터 10까지의 수 중에서 랜덤으로 뽑기
+		int range = 10;
+		int start = 1;
+		int a, b, c;
+		
+		// Math.random()은 0.0 ~ 0.9999999...
+		// Math.random()*range는 0.0 ~ 9.99999999...
+		// Math.random()*range+start는 0.0 ~ 10.99999999...
+		// int로 정수로 변환 1~10
+		
+			
+		a = (int)(Math.random()*range+start);
+		b = (int)(Math.random()*range+start);
+		c = (int)(Math.random()*range+start);
+		
+		
+		// 1부터 10까지의 정수를 랜덤으로 뽑아오는데 세 개의 수가 서로 겹치지 않도록 출력
+		
+		while (true) {
+			
+			if (a==b || b==c || a==c) {
+				continue;
+			}
+			
+			if (a!=b && b!=c && a!=c)
+				System.out.printf("%d, %d, %d", a, b, c);
+				break;
+			
+		}
+		
+		
+	}
+}
